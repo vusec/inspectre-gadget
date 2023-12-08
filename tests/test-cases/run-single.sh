00c3475542a1df23305542fd9bf2cf7c69239710
Sander Wiebing <45387038+SanWieb@users.noreply.github.com>
2023-12-08 11:00:13 +0100

    [Tests] Output asm with run_single

--- tests/test-cases/run-single.sh
+++ tests/test-cases/run-single.sh
@@ -6,8 +6,9 @@ fi
 
  f=$1/gadget
     echo "\n================= $f ================\n"
+    name=`echo $1 | sed 's/\/*$//g'`
     cat $f.s | batcat -l asm
     echo "\n"
     objdump --adjust-vma=0x4000000 -d -Mintel $f | batcat -l asm
     echo ""
-    python3 ../../inspectre analyze --config config_all.yaml --address 0x4000000 --name $1 --output output/gadgets.csv $f
+    python3 ../../inspectre analyze --config config_all.yaml --address 0x4000000 --name $name --output output/gadgets.csv --asm output/asm $f
