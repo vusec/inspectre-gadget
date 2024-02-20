8f97a1d583de70a450af7d003f38bb11c21f4991
Alvise de Faveri Tron <elvisilde@gmail.com>
2024-02-20 01:52:11 +0100

    [queries] minor updates to linux experiment queries

--- experiments/linux/scripts/run-queries.sh
+++ experiments/linux/scripts/run-queries.sh
@@ -6,3 +6,9 @@ echo "[-] Get stats for exploitable gadgets"
 sqlite3 gadgets.db -cmd '.mode table' < queries/exploitable_stats.sql
 echo "[-] Get stats for non-exploitable gadgets"
 sqlite3 gadgets.db -cmd '.mode table' < queries/non_exploitable_stats.sql
+echo "[-] Get stats for fineibt gadgets"
+sqlite3 gadgets.db -cmd '.mode table' < queries/fineibt_gadgets.sql
+echo "[-] Get stats for tfps"
+sqlite3 gadgets.db -cmd '.mode table' < queries/exploitable_tfps.sql
+echo "[-] Get stats for exploitable slam gadgets"
+sqlite3 gadgets.db -cmd '.mode table' < queries/exploitable_slam.sql
