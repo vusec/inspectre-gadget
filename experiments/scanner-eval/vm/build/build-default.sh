4ae354b3365bbcbfa377637c4fce200f836beb4d
Alvise de Faveri Tron <elvisilde@gmail.com>
2024-03-25 16:41:26 +0000

    [ae] Move linux kernel build to Dockerfile

--- experiments/scanner-eval/vm/build/build-default.sh
+++ experiments/scanner-eval/vm/build/build-default.sh
@@ -1,9 +1,5 @@
 set -e
 
-wget https://github.com/torvalds/linux/archive/refs/tags/v6.6-rc4.tar.gz
-tar -xvf v6.6-rc4.tar.gz
-cd linux-6.6-rc4
-
 make defconfig
 
 echo "CONFIG_CONFIGFS_FS=y" >> .config
