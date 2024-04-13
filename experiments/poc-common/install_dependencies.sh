367032f4bb53c4597f217bb5d28f76708c6fcefa
Sander Wiebing <45387038+SanWieb@users.noreply.github.com>
2024-04-13 16:01:28 +0200

    [experiments] Add extra required packages

--- experiments/poc-common/install_dependencies.sh
+++ experiments/poc-common/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install dependencies to build kernel
-sudo apt install build-essential libncurses-dev bison flex libssl-dev libelf-dev zstd liblz4-tool
+sudo apt install build-essential libncurses-dev bison flex libssl-dev libelf-dev zstd liblz4-tool dwarves gdb
 
 # Required for FineIBT testing
 sudo apt install gcc-9
